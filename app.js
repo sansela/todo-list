186a5a370035216ff5ae150674a5cbc77ff14117
sansela <santosh.sela@gmail.com>
2022-12-05 10:36:45 +0530

    Add Procfile

--- app.js
+++ app.js
@@ -126,6 +126,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.port || 3000, function() {
+  console.log("Server started successfully");
 });
